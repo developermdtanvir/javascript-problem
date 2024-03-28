8c1e9ed6f47f1c8108d40cea405acdc6bf76b511
developermdtanvir <officialtanvir@hotmail.com>
2024-03-28 13:09:02 +0600

    add:all javascript code

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+
+
+const PrintParametterName =(message,number) =>{
+    if(number>=70){
+
+        console.log(message+' Is a Good Student');
+    }else{
+        console.log(message+' Is a Bad Boy')
+    }
+};
+
+
+// printParametterName('Tanvir Hossain',50);
+PrintParametterName('Rakib Hossain',70);
+// printParametterName('Tanvir Hossain',50);
